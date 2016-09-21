ebc348aa1c373f74248b8e64f46bc234a9b4741a
sanjeebpradhan <sanjeeb.pradhanktp@gmail.com>
2016-09-21 17:32:00 +0530

    test changes

--- TestWSDL/src/com/test/endpoint/GreetingServiceEndpoint.java
+++ TestWSDL/src/com/test/endpoint/GreetingServiceEndpoint.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import com.javacodegeeks.jaxwsspring.services.GreetingService;
 
 @WebService(serviceName="GreetingService")
-public class GreetingServiceEndpoint extends SpringBeanAutowiringSupport {
+public class GreetingServiceTestEndpoint extends SpringBeanAutowiringSupport {
 
  @Autowired
  private GreetingService greetingService;
