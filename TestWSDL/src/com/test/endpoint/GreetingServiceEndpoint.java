7c0597e7b16566d9b9dcf14f7cb355c9917a998f
sanjeebpradhan <sanjeeb.pradhanktp@gmail.com>
2016-09-21 17:50:27 +0530

    csc commit

--- TestWSDL/src/com/test/endpoint/GreetingServiceEndpoint.java
+++ TestWSDL/src/com/test/endpoint/GreetingServiceEndpoint.java
@@ -9,7 +9,7 @@ import org.springframework.web.context.support.SpringBeanAutowiringSupport;
 import com.javacodegeeks.jaxwsspring.services.GreetingService;
 
 @WebService(serviceName="GreetingService")
-public class GreetingServiceTestEndpoint extends SpringBeanAutowiringSupport {
+public class GreetingServiceCSCEndpoint extends SpringBeanAutowiringSupport {
 
  @Autowired
  private GreetingService greetingService;
